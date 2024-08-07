1120cdad0e2de58fd522920488240ea46edecfe8
Umair Khan <Umair.khan@students.mq.edu.au>
2024-08-07 11:23:28 +1000

    week 2 class work

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
+/* 
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World");
     }
 }
+*/ 
+
+/* Task 3 */ 
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame { 
+    public static void main (String[] args) throws Exception {
+        Main window = new Main();
+    }
+
+    Grid grid;
+
+    class Canvas extends JPanel { 
+        public Canvas () { 
+            setPreferredSize(new Dimension(720, 720));
+            grid = new Grid(); 
+        }
+
+        @Override
+        public void paint(Graphics g) { 
+            grid.paint(g, getMousePosition()); 
+        }
+    }
+
+    private Main() { 
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas(); 
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(true); 
+    }
+
+    public void run() { 
+        while (true) {
+            this.repaint();
+            
+        }
+    }
+
+}
+
+/* Task 4 
+
+
+
+
+
+*/  
+
---
+++ src/grid.java
@@ -0,0 +1,19 @@
+import java.awt.*; 
+
+class Grid { 
+    Cell[][] cells = new Cell[20][20]; 
+    public Grid(){ 
+        for(int i=0; i< cells.length; i++) {
+            for(int j=0; j< cells[i].length; j++) { 
+                cells[i][j] = new Cell(10+35*i,10+35*j);
+            }
+        }
+    }
+public void paint(Graphics g, Point mousePos){ 
+    for(int i=0; i< cells.length; i++) {
+        for(int j=0; j< cells[i].length; j++) { 
+            cells[i][j].paint(g, mousePos);
+            }
+        }
+    }
+} 
