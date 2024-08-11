47ff2a313aac168bed5814652975f185dffac3ba
Umair Khan <Umair.khan@students.mq.edu.au>
2024-08-11 23:04:32 +1000

    UK 11/08: actual commmit for part 2 of milestone 1

--- src/Main.java
+++ src/Main.java
@@ -7,68 +7,64 @@ import java.util.Queue;
 
 
 public class Main extends JFrame {
-    public static void main(String[] args) throws Exception {
-        Main window = new Main();
-        window.run();
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::new); // calls the main constructor initializing the thinamabob
     }
 
 
     class Canvas extends JPanel {
-        Grid grid = new Grid();
-        private final int TRAIL_SIZE = 100; // makes it so that the max amount of dots is 100
+        private static final int TRAIL_SIZE = 100;
         private final Queue<Point> mouseTrails = new LinkedList<>();
+        private final Grid grid = new Grid();
+        private boolean mouseMoved = false; // true or false if mouse moved
 
 
         public Canvas() {
             setPreferredSize(new Dimension(720, 720));
-
-
-            // Mouse motion listener to capture mouse movements
             addMouseMotionListener(new MouseAdapter() {
                 @Override
                 public void mouseMoved(MouseEvent e) {
+                    mouseMoved = true;
                     if (mouseTrails.size() >= TRAIL_SIZE) {
-                        mouseTrails.poll(); // Remove oldest position if we have reached max size
+                        mouseTrails.poll(); // makes sure the dots dont go past 100
                     }
-                    mouseTrails.add(e.getPoint()); // Adds current position to the list
-                    repaint(); // repaints the thing so it can show the trail
+                    mouseTrails.add(e.getPoint()); // finds mouse pos
+                    repaint(); //  repaints the thing so it can show the trail
                 }
             });
+
+
+           
+            Timer timer = new Timer(100, e -> { // used to see if the mouse is moving, if not it clears the trail
+                if (!mouseMoved) {
+                    mouseTrails.clear(); // Clear trails if mouse hasn't moved
+                    repaint(); // Repaint to update the display
+                }
+                mouseMoved = false; // Resets the boolean if statment
+            });
+            timer.start();
         }
 
 
         @Override
-        public void paint(Graphics g) {
-            super.paint(g); // Makes it so the background is always painted making show the trail on top
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g); // Ensure the background is cleared
             grid.paint(g, getMousePosition());
 
 
-            g.setColor(new Color(13, 168, 189)); // Sets the trail to blue-ish
-            for (Point p : mouseTrails) {
+            // Draw the mouse trails
+            g.setColor(new Color(62, 62, 62)); // sets the colour to something thats really close to 44, 44, 44
+            for (Point p : mouseTrails) { // i hate this if statement
                 if (p != null) {
-                    g.fillOval(p.x - 10, p.y - 10, 20, 20); // draws the cirlce at the mouse
+                    g.fillOval(p.x - 10, p.y - 10, 20, 20); // Draw circle centered at the mouse position
                 }
             }
         }
     }
-
-
-
-
     private Main() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Canvas canvas = new Canvas();
-        this.setContentPane(canvas);
-        this.pack();
-        this.setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(new Canvas());
+        pack();
+        setVisible(true);
     }
-
-
-    public void run() {
-       
-    }
-
-
-
-
 }
